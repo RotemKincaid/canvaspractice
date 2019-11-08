065a01522333133095a77e327a17e5287576ffac
Rotem <rotem.kincaid@nikolamotor.com>
2019-11-08 14:54:27 -0700

    Wrote pie function to create it

--- graphs/pie.js
+++ graphs/pie.js
@@ -1,3 +1,10 @@
-$(function() {
+function createCircle(x, y, radius, startAngle, endAngle, pie) {
+    var c = document.getElementById(pie)
+    var ctx = c.getContext("2d")
+    ctx.beginPath() 
+    ctx.arc(x, y, radius, startAngle, endAngle * Math.PI)
+    ctx.stroke()
+}
 
-});
+
+createCircle(295, 245, 240, 0, 2, "pie")
